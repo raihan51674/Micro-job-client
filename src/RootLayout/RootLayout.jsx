8204262ae981049ddcbb655ebcfb003fbf3a68d5
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-07-10 20:06:38 +0600

    start micro-job project

---
+++ src/RootLayout/RootLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <h2>Hello Developer</h2>
+        </div>
+    );
+};
+
+export default RootLayout;
---
+++ src/Router/router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../RootLayout/RootLayout";
+
+
+
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+  },
+]);
